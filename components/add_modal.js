f9837e8f2bc0231e20fcc3aa2ba8be59d988e00f
yeonjun <yeonjunky@gmail.com>
2022-02-24 20:13:26 +0900

    add modal done

--- components/add_modal.js
+++ components/add_modal.js
@@ -1,22 +1,92 @@
-import { StyleSheet, Text, View, FlatList, Modal, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Modal, TextInput, Pressable } from 'react-native';
 import { useState } from 'react'; 
 
-export default function AddModal() {
-  const [ modalVisible, setModalVisible ] = useState(false);
+export default function AddModal(props) {
   const [ text, setText ] = useState('');
 
   return (
-    <Modal
-      animationType='slide'
-      transparent='true'
-      visible={modalVisible}
-      onRequestClose={setModalVisible(!modalVisible)}
-    >
-      <TextInput 
-        value={text}
-        onChangeText={setText}
-        placeholder='Name'
-      />
-    </Modal>
+    <View style={styles.center}>
+      <Modal
+        animationType='fade'
+        transparent={true}
+        visible={props.modalVisible}
+        onRequestClose={() => props.onRequestClose(!props.modalVisible)}
+        >
+        <View style={styles.modalView}>
+          <View style={styles.modalInput}>
+            <TextInput 
+              value={text}
+              onChangeText={setText}
+              placeholder='Name'
+            />
+          </View>
+
+          <View style={styles.modalInput}>
+            <TextInput 
+              value={text}
+              onChangeText={setText}
+              placeholder='Name'
+            />
+          </View>
+
+          <View style={styles.buttonView}>
+
+            <View style={styles.button}>
+              <Pressable>
+                <Text style={styles.text}>
+                  Apply
+                </Text>
+              </Pressable>
+            </View>
+
+            <View style={styles.button}>
+              <Pressable onPress={() => props.onRequestClose(!props.modalVisible)}>
+                <Text style={styles.text}>
+                  Cancel
+                </Text>
+              </Pressable>
+            </View>
+
+          </View>  
+        </View>
+      </Modal>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  modalView: {
+      flexDirection: 'column',
+      width: '65%',
+      alignSelf: 'center',
+      marginTop: '40%',
+      elevation: 3,
+      backgroundColor: '#fff',
+  },
+  modalInput: {
+    borderBottomColor: '#35baf6',
+    borderBottomWidth: 1,
+    margin: 20,
+  },
+  buttonView: {
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 20,
+    marginRight: 5,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 2,
+  },
+  text: {
+    fontSize: 15,
+    margin: 10,
+  },
+})
